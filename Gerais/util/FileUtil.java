c276bace01bbb4d79f93602d608e0553378eadb7
Leonardo Silva <macenaleonardo2@gmail.com>
2026-06-10 19:39:39 -0300

    Delete Gerais directory

--- Gerais/util/FileUtil.java
+++
@@ -1,5 +0,0 @@
-package util;
-public class FileUtil {
-    public static void gravar(String arquivo, String conteudo) { /* Implementação de escrita */ }
-    public static String ler(String arquivo) { return ""; }
-}
